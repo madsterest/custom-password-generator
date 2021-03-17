a6d12f692ff30822e9ff08225965e1c6260c4651
madsterest <odea.madeleine@gmail.com>
2021-03-18 09:32:04 +1100

    Cleaned up the function. Added code for when the user inputs an invalid answer. Put the characters into a string that is split instead of storing them in arrays

--- Develop/script.js
+++ Develop/script.js
@@ -10,108 +10,23 @@ function writePassword() {
     var newPassword = [];
     var length = 8;
     var passwordCharacters = [];
-    var letters = [
-      "a",
-      "b",
-      "c",
-      "d",
-      "e",
-      "f",
-      "g",
-      "h",
-      "i",
-      "j",
-      "k",
-      "l",
-      "m",
-      "n",
-      "o",
-      "p",
-      "q",
-      "r",
-      "s",
-      "t",
-      "u",
-      "v",
-      "w",
-      "x",
-      "y",
-      "z",
-    ];
-    var upperLetters = [
-      "A",
-      "B",
-      "C",
-      "D",
-      "E",
-      "F",
-      "G",
-      "H",
-      "I",
-      "J",
-      "K",
-      "L",
-      "M",
-      "N",
-      "O",
-      "P",
-      "Q",
-      "R",
-      "S",
-      "T",
-      "U",
-      "V",
-      "W",
-      "X",
-      "Y",
-      "Z",
-    ];
-    var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-    var symbols = [
-      "!",
-      '"',
-      "#",
-      "$",
-      "%",
-      "&",
-      "'",
-      "(",
-      ")",
-      "*",
-      "+",
-      ",",
-      "-",
-      ".",
-      "/",
-      ":",
-      ";",
-      "<",
-      "=",
-      ">",
-      "?",
-      "@",
-      "[",
-      "]",
-      "^",
-      "_",
-      "`",
-      "{",
-      "|",
-      "}",
-      "~",
-    ];
+    var letters = "abcdefghijklmnopqrstuvwxyz".split("");
+    var upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+    var numbers = "1234567890".split("");
+    var symbols = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~" + '"'.split("");
 
     //Sets the length of the password. Has a minimun length of 8
     var passwordLength = window.prompt(
-      "How long would you like your password? (Must be at least 8 characters)"
+      "How long would you like your password? (Must be between 8 and 128 characters)"
     );
-    //If the password is shorter than 8, they must input a new number. Prompts come back as strings, so I convert it to a number incase it interferes with the for loop.
-    if (passwordLength < 8) {
-      window.prompt("Password must be at least 8 characters");
+    //If the password is shorter than 8 or longer than 128 characters, the function starts again until they pick a valid password length. Prompts come back as strings, so I convert it to a number incase it interferes with the for loop.
+    if (passwordLength < 8 || passwordLength > 128) {
+      window.alert("Password must be between 8 and 128 characters long");
+      return generatePassword();
     } else {
       length = Number(passwordLength);
     }
-    //selects whether they want upper or lower case letters in their password. If they answer neither, they go back to the beginning.
+    //selects whether they want upper or lower case letters in their password. If they answer neither, they go back to the start of the function.
     var lower = window.confirm("Do you want lower case letters?");
     if (lower) {
       passwordCharacters.push(...letters);
@@ -120,10 +35,10 @@ function writePassword() {
     if (upper) {
       passwordCharacters.push(...upperLetters);
     }
-    // if (!upper && !lower) {
-    //   window.alert("Must contain either lower or upper case letters");
-    //   generatePassword();
-    // }
+    if (!upper && !lower) {
+      window.alert("Must contain either lower or upper case letters");
+      return generatePassword();
+    }
     var numerical = window.confirm("Do you want Numbers?");
     if (numerical) {
       passwordCharacters.push(...numbers);
